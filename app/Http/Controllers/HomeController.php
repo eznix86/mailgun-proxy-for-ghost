8fea7289bc70f19896927e902815e87209bda710
Bruno Bernard <contact.brunobernard@gmail.com>
2026-05-01 00:15:36 +0400

    style: lint it

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,6 @@ class HomeController extends Controller
 {
     public function __invoke(): RedirectResponse
     {
-        return redirect()->route('register');
+        return to_route('register');
     }
 }
